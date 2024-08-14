1f3068c11b28ab88ec36a0d561e8786cef551033
Buddika <buddikab97@outlook.com>
2024-08-14 11:01:47 +0530

    use select command

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,19 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String sqlStatement="delete Customer where name='Jinasena'";
+        String sqlStatement="select * from Customer";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/thogaKade", "root", "root");
             Statement stm= connection.createStatement();
-            int res=stm.executeUpdate(sqlStatement);
-            if(res>0){
-                System.out.println("Delete success!!!");
-                System.out.println("No if row count " + res);
-            }
+            ResultSet res=stm.executeQuery(sqlStatement);
+            res.next();
+            String id = res.getString("id");
+            String name = res.getString("name");
+            String address = res.getString("address");
+            String salary = res.getString("salary");
+
+            System.out.println(id + " " + name + " " + address + " " + salary);
         } catch (ClassNotFoundException ex) {
             System.out.println("Driver s/w not found");
             System.out.println(ex);
